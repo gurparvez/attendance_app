1b99f3c1b6f751ae63a34d32fde6f72c84b2d199
gurparvez <gurparvez04@gmail.com>
2024-09-05 15:59:46 +0530

    login validation

--- lib/src/views/screens/login/login.dart
+++ lib/src/views/screens/login/login.dart
@@ -9,8 +9,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController idController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  Map<String, String> formData = {
+    "id": "",
+    "password": "",
+  };
+  final formKey = GlobalKey<FormState>();
   bool isPasswordVisible = false;
   bool isLoading = false;
 
@@ -20,77 +23,106 @@ class _LoginPageState extends State<LoginPage> {
     });
   }
 
+  void login() {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState?.save();
+      print(formData);
+      setState(() {
+        isLoading = !isLoading;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 40),
-                  Text(
-                    'Akal University Attendance',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.indigo[900],
+          Form(
+            key: formKey,
+            child: Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 40),
+                    Text(
+                      'Akal University Attendance',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigo[900],
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                  // Image(image: AssetImage("assets/images/logo.png")), // TODO: add uni logo
-                  const SizedBox(height: 50),
-                  TextField(
-                    controller: idController,
-                    decoration: InputDecoration(
-                      labelText: 'User ID',
-                      hintText: 'Enter User ID',
-                      prefixIcon: const Icon(Icons.person_outline),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
+                    // Image(image: AssetImage("assets/images/logo.png")), // TODO: add uni logo
+                    const SizedBox(height: 50),
+                    TextFormField(
+                      onSaved: (value) {
+                        formData["id"] = value.toString();
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your id";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'User ID',
+                        hintText: 'Enter User ID',
+                        prefixIcon: const Icon(Icons.person_outline),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: !isPasswordVisible,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter Password',
-                      prefixIcon: const Icon(Icons.lock_outline),
-                      suffixIcon: IconButton(
-                        onPressed: toggleShowPassword,
-                        icon: isPasswordVisible
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      onSaved: (value) {
+                        formData["password"] = value.toString();
+                      },
+                      obscureText: !isPasswordVisible,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your id";
+                        }
+                        if (value.length < 8) {
+                          return "Password should be of 8 characters";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter Password',
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          onPressed: toggleShowPassword,
+                          icon: isPasswordVisible
+                              ? const Icon(Icons.visibility_off)
+                              : const Icon(Icons.visibility),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 24),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ButtonTextPrimary(
-                        text: "Log In",
-                        onPressed: () {
-                          setState(() {
-                            isLoading = !isLoading;
-                          });
-                        },
-                        isLoading: isLoading,
-                      )
-                    ],
-                  ),
-                ],
+                    const SizedBox(height: 24),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ButtonTextPrimary(
+                          text: "Log In",
+                          onPressed: login,
+                          isLoading: isLoading,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
